691a5d7db9276876bffd5931a9c2e83ad4877e2d
nicogravel <nicolas.gravel@gmail.com>
2025-03-30 20:51:52 +0200

    typesetting and layout changes

--- docs_local/source/conf.py
+++ docs_local/source/conf.py
@@ -146,11 +146,6 @@ git commit -m "typesetting and layout changes"
 git push -u origin main
 
 
-'''
-
-
-'''
-
 # Create an animated GIF from a series of images in a directory
 
 pth='/home/nicolas/Documents/GitHubProjects/researchLog_template/docs_local/source/figures/coReg/'
@@ -171,4 +166,5 @@ convert -delay 100 -loop 0 sub-00_iso_run*_T2_T1_alignment.png animated_alignmen
 echo "Animated GIF saved as animated_alignment.gif in $pth"'
 ''
 
-''''
+'''
+
